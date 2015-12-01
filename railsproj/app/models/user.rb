dcf2fb9f74004decddd57d5b413f8f4082ab33b8
Kevin Lee <kevin651102@berkeley.edu>
2015-12-01 15:38:01 -0800

    Working Commit

--- railsproj/app/models/user.rb
+++ railsproj/app/models/user.rb
@@ -5,6 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :group
   has_one :status
+  after_create :create_status
   geocoded_by :address
+  after_initialize :default_address
   after_validation :geocode
+  has_attached_file :photo, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
+  def default_address
+  	self.address ||= 'Berkeley, CA'
+  end
 end
--- railsproj/db/schema.rb
+++ railsproj/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130104253) do
+ActiveRecord::Schema.define(version: 20151201093254) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20151130104253) do
     t.float    "longitude"
     t.string   "address"
     t.integer  "group_id"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
